1e910a294ffbefae1a5a51a34ec371cf23064d87
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2025-06-01 12:41:40 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// -----------
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.snapshot = {
+      ...config.snapshot,
+      managedPaths: [
+        ...(config.snapshot?.managedPaths || []),
+        'C:\\Users\\haris\\Application Data',
+        'C:\\Users\\haris\\Cookies',
+      ],
+    };
+
+    config.watchOptions = {
+      ignored: [
+        '**/node_modules',
+        'C:/Users/haris/Application Data',
+        'C:/Users/haris/Cookies',
+      ],
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
