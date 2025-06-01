75bba5afa354c3fbbc0ddb6a6fdc0b3636a85135
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2025-06-01 13:05:26 +0530

    lint fix

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-// -----------
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.snapshot = {
-      ...config.snapshot,
-      managedPaths: [
-        ...(config.snapshot?.managedPaths || []),
-        'C:\\Users\\haris\\Application Data',
-        'C:\\Users\\haris\\Cookies',
-      ],
-    };
-
-    config.watchOptions = {
-      ignored: [
-        '**/node_modules',
-        'C:/Users/haris/Application Data',
-        'C:/Users/haris/Cookies',
-      ],
-    };
-
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
